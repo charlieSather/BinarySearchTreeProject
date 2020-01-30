e31e8190758f6cf8139c50d859515c7770e41174
unknown <charliesather18@gmail.com>
2020-01-30 13:25:28 -0600

    Finished Add() and Search() methods for an BST that can contain IComparables.

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable
+    { 
+        Node<T> root;
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if(root is null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node<T> current = root;
+                while(current != null)
+                {
+                    if (data.CompareTo(current.data) <= 0)
+                    {
+                        current = current.leftChild;
+                    }
+                    else
+                    {
+                        current = current.rightChild;
+                    }
+                }
+                current = node;               
+            }           
+        }
+        public Node<T> Search(T data)
+        {
+            Node<T> current = root;
+            
+            while(current != null)
+            {
+                if (current.data.Equals(data))
+                {
+                    return current;
+                }
+                else if(data.CompareTo(current.data) < 0)
+                {
+                    current = current.leftChild;
+                }
+                else
+                {
+                    current = current.rightChild;
+                }
+            }
+            return null;
+        }
+
+        
+      
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node<T> where T : IComparable
+    {
+        public T data;
+        public Node<T> leftChild;
+        public Node<T> rightChild;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+
+    }
+}
